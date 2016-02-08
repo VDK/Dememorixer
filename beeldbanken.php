3ca932a9579ad6f36d97742b9710de268ebc7d8e
Vera de Kok <veradekok@gmail.com>
2016-02-08 21:33:37 +0100

    Create beeldbanken.php

---
+++ beeldbanken.php
@@ -0,0 +1,12 @@
+<?php
+$beeldbanken = array();
+array_push($beeldbanken, array(
+		"url" => "koninklijkeverzamelingen\.nl\/collectie-online", 
+		"abc" => "kha" 
+		));
+array_push($beeldbanken, array(
+	"url" => "beeldbankgroningen\.nl\/beelden", 
+	"abc" => "gra" 
+	));
+
+?>
